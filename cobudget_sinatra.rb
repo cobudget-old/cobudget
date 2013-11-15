e8a5d0733ef55dd0b972f12484927bba44e41a54
Joshua Vial <joshua@enspiral.com>
2013-11-15 13:35:24 +1000

    cobudget sinatra file

---
+++ cobudget_sinatra.rb
@@ -0,0 +1,13 @@
+$LOAD_PATH << File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
+require 'sinatra/base'
+require 'playhouse/sinatra'
+require 'cobudget/api'
+
+class CobudgetWeb < Sinatra::Base
+  register Playhouse::Sinatra
+  #set :root,  File.expand_path(File.join(File.dirname(__FILE__)))
+
+  add_play Cobudget::API
+
+  run! if app_file == $0
+end
