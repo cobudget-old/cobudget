9040f107065f50d07346a473e719f6cfaa278ae4
Joshua Vial <joshua@enspiral.com>
2013-11-18 06:39:04 +1300

    cap task to reset database

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,16 +25,25 @@ namespace :site do
   task :symlink do
     run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
   end
+  task :reset_db do
+    rake_task("db:reset")
+    rake_task("db:seed")
+  end
 end
 
 namespace :rake do
   desc "Run a task on a remote server."
   # run like: cap staging rake:invoke task=a_certain_task
   task :invoke do
-    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} ENV=#{rails_env}")
+    rake_task(ENv['task'])
   end
 end
 
 require "bundler/capistrano"
 after "deploy:restart", "deploy:cleanup"
 after "deploy:update_code", "site:symlink"
+
+def rake_task(task)
+  run("cd #{deploy_to}/current; /usr/bin/env rake #{task} ENV=#{rails_env}")
+end
+
