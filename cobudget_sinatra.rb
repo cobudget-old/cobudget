1db89d05e4ea4fef2fc2604b8b330e93e1768efa
Joshua Vial <joshua@enspiral.com>
2013-11-18 06:32:39 +1300

    fixing access control allow origin

--- cobudget_sinatra.rb
+++ cobudget_sinatra.rb
@@ -6,6 +6,9 @@ require 'cobudget/api'
 class CobudgetWeb < Sinatra::Base
   register Playhouse::Sinatra
   #set :root,  File.expand_path(File.join(File.dirname(__FILE__)))
+  before do
+    headers 'Access-Control-Allow-Origin' => '*'
+  end
 
   add_play Cobudget::API
 
