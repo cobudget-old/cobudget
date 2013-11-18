3870104033e2180dd0d7fe516586b74f52028840
allansideas <allansideas@gmail.com>
2013-11-18 20:29:16 +0100

    better CORS stuff, but not quite good enough perhaps?

--- cobudget_sinatra.rb
+++ cobudget_sinatra.rb
@@ -7,7 +7,13 @@ class CobudgetWeb < Sinatra::Base
   register Playhouse::Sinatra
   #set :root,  File.expand_path(File.join(File.dirname(__FILE__)))
   before do
-    headers 'Access-Control-Allow-Origin' => '*'
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, content-type, Cache-Control, accept"
+    headers["Allow"] = "HEAD,GET,POST,PUT,DELETE,OPTIONS"
+    if request.request_method == 'OPTIONS'
+      headers["Access-Control-Allow-Methods"] = "POST, GET"
+      halt 200
+    end
   end
 
   add_play Cobudget::API
