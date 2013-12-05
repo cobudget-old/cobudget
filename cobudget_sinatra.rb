5935b611d17d36038eac8ec37955df82827bb864
Joshua Vial <joshua@enspiral.com>
2013-12-05 20:43:16 +1300

    playing with CORS

--- cobudget_sinatra.rb
+++ cobudget_sinatra.rb
@@ -8,11 +8,11 @@ class CobudgetWeb < Sinatra::Base
   #set :root,  File.expand_path(File.join(File.dirname(__FILE__)))
   before do
     headers "Access-Control-Allow-Origin" => "*"
-    #headers["Allow"] = "GET,POST,PUT,DELETE"
-    #if request.request_method == 'OPTIONS'
-      #headers "Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Methods" => "POST, GET", "Access-Control-Allow-Headers" => "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept, Origin"
-      #status 200
-    #end
+    headers["Allow"] = "GET,POST,PUT,DELETE"
+    if request.request_method == 'OPTIONS'
+      headers "Access-Control-Allow-Origin" => "*", "Access-Control-Allow-Methods" => "POST, GET", "Access-Control-Allow-Headers" => "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept, Origin"
+      status 200
+    end
   end
 
   add_play Cobudget::API
