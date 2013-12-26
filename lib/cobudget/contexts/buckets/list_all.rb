03ba3321656ab379177de79153857e8121a19961
ruth-ablett <ruthablett@gmail.com>
2013-12-26 18:36:55 +1300

    ffs

---
+++ lib/cobudget/contexts/buckets/list_all.rb
@@ -0,0 +1,21 @@
+require 'playhouse/context'
+require 'cobudget/entities/budget'
+require 'cobudget/entities/bucket'
+require 'cobudget/entities/user'
+
+module Cobudget
+  module Buckets
+    class ListAll < Playhouse::Context
+      class NotAuthorizedToListAllBuckets < Exception; end
+
+      actor :budget, repository: Budget
+      actor :admin, repository: User
+
+      def perform
+        #raise NotAuthorizedToListAllBuckets if admin.can_list_all_buckets?
+
+        budget.buckets.order('updated_at desc').load
+      end
+    end
+  end
+end
