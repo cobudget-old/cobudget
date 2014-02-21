e9599f5803a78ed2ff7790e32ce371977477442e
allansideas <allansideas@gmail.com>
2014-02-21 21:52:10 +0100

    woop

--- lib/cobudget/contexts/users/authenticate.rb
+++ lib/cobudget/contexts/users/authenticate.rb
@@ -10,7 +10,8 @@ module Cobudget
       def perform
         user = User.find_by_email(email)
         if user
-          user['last_sign_in_at'] = Time.now
+          user.last_sign_in_at = Time.now
+          user.save
           user.as_json
         else
           User.create(actors).as_json
