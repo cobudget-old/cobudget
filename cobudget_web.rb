8c0394b110ba3e444cf9257b86ce2d9b4b2031cb
Joshua Vial <joshua@enspiral.com>
2014-05-03 13:05:42 -0700

    cleaning up cobudget_web

--- cobudget_web.rb
+++ cobudget_web.rb
@@ -4,21 +4,13 @@ require 'logger'
 require 'sinatra/base'
 require 'playhouse/sinatra'
 require 'cobudget/cobudget_play'
-require 'cobudget/production'
-require 'cobudget_theatre'
 
 class CobudgetWeb < Sinatra::Base
   use Airbrake::Rack
   enable :raise_errors
 
-  #production = Cobudget::Production.new
-  #theatre = Cobudget::CobudgetTheatre.new(root: settings.root, environment: settings.environment)
-
-  #production.run theatre: theatre, interface: Playhouse::Sinatra::Interface, interface_args: ARGV
-
   register Playhouse::Sinatra
   set :root,  File.expand_path(File.join(File.dirname(__FILE__)))
-  #disable :protection
   set :protection, :except => [:http_origin]
   routes = YAML.load_file('config/routes.yml')
 
@@ -27,18 +19,8 @@ class CobudgetWeb < Sinatra::Base
     use Rack::CommonLogger, $logger
   end
 
-  production = Cobudget::Production.new
-  production.routes = routes
   theatre = Cobudget::CobudgetTheatre.new(root: settings.root, environment: settings.environment)
-  #root: settings.root, environment: settings.environment
-
   add_play theatre, Cobudget::CobudgetPlay, routes
 
-  #production.run(theatre: theatre, interface: nil, interface_args: ARGV )
-
   run! if app_file == $0
-
-  def build_cobudget_web theatre
-
-  end
 end
