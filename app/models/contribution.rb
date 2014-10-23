fc89b7b829b52505cc477417d1fe0f9a487a05a7
Jon Lemmon <jon@loomio.org>
2014-10-23 15:32:19 +1300

    users can only have ONE contribution PER bucket

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -3,7 +3,7 @@ class Contribution < ActiveRecord::Base
   belongs_to :user
 
   validates :bucket_id, presence: true
-  validates :user_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :bucket_id }
 
   def self.for_round(round_id)
     joins(bucket: :round).where('buckets.round_id = ?', round_id)
