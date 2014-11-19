0684f0076b1b5072f6c5f7e57e94b21e868f081a
Jon Lemmon <jon@loomio.org>
2014-11-20 10:59:56 +1300

    Group serializer: Add basic rounds info

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,4 +1,5 @@
 class GroupSerializer < ActiveModel::Serializer
   attributes :id, :name, :latest_round_id
   has_one :latest_round
+  has_many :rounds, serializer: RoundShortSerializer
 end
---
+++ app/serializers/round_short_serializer.rb
@@ -0,0 +1,3 @@
+class RoundShortSerializer < ActiveModel::Serializer
+  attributes :id, :name, :created_at
+end
