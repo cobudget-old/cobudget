75205b7948d2ef58d73dee16be0dc976c79909c5
Michael Williams <dinosaur@riseup.net>
2014-11-29 14:23:49 -0800

    add POST /buckets

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -4,8 +4,22 @@ class BucketsController < ApplicationController
     respond_with bucket
   end
 
+  api :POST, '/buckets/', 'Create a bucket'
+  def create
+    # TODO: make sure only group admins can create new buckets
+    respond_with Bucket.create(bucket_params)
+  end
+
   private
     def bucket
       @bucket ||= Bucket.find(params[:id])
+    end
+
+    def bucket_params
+      params.require(:bucket).permit(:name,
+                                     :description,
+                                     :round_id,
+                                     :user_id,
+                                     :target_cents)
     end
 end
