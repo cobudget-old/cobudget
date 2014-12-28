c96c8e574e914b49c9fee7ab0af9dfe2bc2ca26b
Jon Lemmon <jon@loomio.org>
2014-12-28 18:26:55 +1300

    Order rounds

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  has_many :rounds, dependent: :destroy
+  has_many :rounds, ->{ order("ends_at DESC") }, dependent: :destroy
   has_one  :latest_round, class_name: 'Round', order: "id DESC"
   has_many :memberships
   has_many :members, through: :memberships, source: :user
