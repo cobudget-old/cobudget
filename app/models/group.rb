59ef9cb00b0ac8664217ed5e6f1d5873e490c204
Jon Lemmon <jon@loomio.org>
2014-12-28 15:21:53 +1300

    Remove latest_round stuff

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,8 +3,4 @@ class Group < ActiveRecord::Base
   has_one  :latest_round, class_name: 'Round', order: "id DESC"
   has_many :memberships
   has_many :members, through: :memberships, source: :user
-
-  def latest_round_id
-    latest_round.id if latest_round
-  end
 end
--- app/serializers/budget_serializer.rb
+++ app/serializers/budget_serializer.rb
@@ -1,3 +1,3 @@
 class BudgetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :latest_round_id
+  attributes :id, :name
 end
