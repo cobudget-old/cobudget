97e14a8d1962cecef98a0c9509659d08a1449fe9
Michael Williams <dinosaur@riseup.net>
2014-12-29 00:19:38 +1300

    add contribution controller policy

---
+++ app/policies/contribution_policy.rb
@@ -0,0 +1,13 @@
+class ContributionPolicy < ApplicationPolicy
+  def create?
+    user.id == record.user.id
+  end
+
+  def update?
+    user.id == record.user.id
+  end
+
+  def destroy?
+    user.id == record.user.id
+  end
+end
