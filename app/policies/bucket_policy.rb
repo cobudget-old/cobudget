90e639568cd3e5b9be8ff8af34f5b4f2ca2f83ab
Michael Williams <dinosaur@riseup.net>
2015-01-19 14:34:07 +1300

    change bucket policies to allow editing by sponsor

--- app/policies/bucket_policy.rb
+++ app/policies/bucket_policy.rb
@@ -1,13 +1,18 @@
 class BucketPolicy < ApplicationPolicy
   def create?
-    user.is_admin_for?(record.round.group)
+    user.is_admin_for?(record.round.group) or (
+      record.round.group.members.include?(user) and
+      record.round.members_can_propose_buckets
+    )
   end
 
   def update?
-    user.is_admin_for?(record.round.group)
+    user.is_admin_for?(record.round.group) or
+      record.user_id == user.id
   end
 
   def destroy?
-    user.is_admin_for?(record.round.group)
+    user.is_admin_for?(record.round.group) or
+      record.user_id == user.id
   end
 end
