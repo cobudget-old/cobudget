77fdc5eb6e79efc4ff0aafbb970bba0e843b4017
euglazer <eugenestapler@gmail.com>
2015-05-19 11:21:47 +1200

    full test coverage on contribution model

--- spec/models/contribution_spec.rb
+++ spec/models/contribution_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Contribution, :type => :model do
-  let(:contribution) { FactoryGirl.create(:contribution) }
 
-  it "validates that allocation is greater than 0" do
-    contribution.amount = BigDecimal.new(0)
-    expect(contribution).to_not be_valid
+	it { should have_db_column(:amount).of_type(:decimal).with_options(precision: 12, scale: 2, default: 0.0) }
+
+  it { should belong_to(:bucket) }
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:bucket_id) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_uniqueness_of(:user_id).scoped_to(:bucket_id) }
+  it { should validate_numericality_of(:amount).is_greater_than(0) }
+
+  describe "#self.for_round(round_id)" do
+  	it "returns all contributions from buckets with specified round_id" do
+  		round1 = create(:round_open_for_contributions)
+  		bucket1 = create(:bucket, round: round1)	
+  		contributions1 = Array.new(5) { create(:contribution, bucket: bucket1) }
+
+  		round2 = create(:round_open_for_contributions)
+  		bucket2 = create(:bucket, round: round2)	
+  		contributions2 = Array.new(5) { create(:contribution, bucket: bucket2) }
+
+  		expect(Contribution.for_round(round1.id)).to eq(contributions1)
+  		expect(Contribution.for_round(round2.id)).to eq(contributions2)
+  	end
   end
+
 end
