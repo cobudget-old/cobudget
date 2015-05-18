43cd9da78ec46c52f9426d55395f7164651e733f
euglazer <eugenestapler@gmail.com>
2015-05-19 11:08:44 +1200

    configure factory girl

--- spec/support/factory_girl_and_db_cleaner.rb
+++ spec/support/factory_girl_and_db_cleaner.rb
@@ -1,6 +1,6 @@
 RSpec.configure do |config|
   # additional factory_girl configuration
-
+  config.include FactoryGirl::Syntax::Methods
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
