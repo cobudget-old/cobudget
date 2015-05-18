b190c098b01764ffdf94792c9579608775fc1a15
euglazer <eugenestapler@gmail.com>
2015-05-19 10:47:15 +1200

    add shoulda matchers to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'shoulda/matchers'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
