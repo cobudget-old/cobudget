f41e12e462db83318314a2bf9e1f089b24b23ded
euglazer <eugenestapler@gmail.com>
2015-05-20 10:48:42 +1200

    change validation to follow convention used in other models

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -3,6 +3,6 @@ class Allocation < ActiveRecord::Base
   belongs_to :user
 
   validates :round_id, presence: true
-  validates :user_id, presence: true, uniqueness: {scope: :round}
+  validates :user_id, presence: true, uniqueness: {scope: :round_id}
   validates :amount, presence: true
 end
