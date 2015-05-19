be9710f1ed8c738d458d5eb7f5df894367240c24
euglazer <eugenestapler@gmail.com>
2015-05-19 23:05:00 +1200

    format round specs

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -2,19 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Round, :type => :model do
 
-  it { should have_db_column(:group_id).with_options(null: false) }
-  it { should have_db_column(:name).of_type(:string).with_options(null: false) }
-  it { should have_db_column(:starts_at).of_type(:datetime) }
-  it { should have_db_column(:ends_at).of_type(:datetime) }
-  it { should have_db_column(:members_can_propose_buckets).of_type(:boolean) }
+  describe "fields" do
+    it { should have_db_column(:group_id).with_options(null: false) }
+    it { should have_db_column(:name).of_type(:string).with_options(null: false) }
+    it { should have_db_column(:starts_at).of_type(:datetime) }
+    it { should have_db_column(:ends_at).of_type(:datetime) }
+    it { should have_db_column(:members_can_propose_buckets).of_type(:boolean) }
+  end
 
-  it { should belong_to(:group) }
-  it { should have_many(:buckets).dependent(:destroy) }
-  it { should have_many(:allocations).dependent(:destroy) }
-  it { should have_many(:fixed_costs).dependent(:destroy) }
+  describe "associations" do
+    it { should belong_to(:group) }
+    it { should have_many(:buckets).dependent(:destroy) }
+    it { should have_many(:allocations).dependent(:destroy) }
+    it { should have_many(:fixed_costs).dependent(:destroy) }
+  end
 
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:group) }
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:group) }
+  end
 
   describe "#start_and_end_go_together" do
     it "validates that both starts_at and ends_at to be present or neither be present" do
@@ -30,7 +36,6 @@ RSpec.describe Round, :type => :model do
       expect { Round.create!(name: 'hi', group: group, starts_at: Time.now + 2.days, ends_at: Time.now + 1.day) }.to raise_error
     end    
   end
-
 
   describe '#open_for_proposals?' do
     it "returns true if starts_at and ends_at are defined and starts_at hasn't happened yet" do
