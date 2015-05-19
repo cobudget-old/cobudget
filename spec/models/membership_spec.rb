e69006e975fe5c3a724fe93a8441a8c56bc3156f
euglazer <eugenestapler@gmail.com>
2015-05-19 22:59:18 +1200

    format membership specs

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -2,18 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Membership, :type => :model do
 
-  it { should have_db_column(:group_id).with_options(null: false) }
-  it { should have_db_column(:member_id).with_options(null: false) }
-  it { should have_db_column(:is_admin).with_options(default: false, null: false) }
+  describe "fields" do
+    it { should have_db_column(:group_id).with_options(null: false) }
+    it { should have_db_column(:member_id).with_options(null: false) }
+    it { should have_db_column(:is_admin).with_options(default: false, null: false) }
+  end
 
-  it { should belong_to(:group) }
-  it { should belong_to(:member).class_name("User") }
-  it { should validate_presence_of(:group_id) }
-  it { should validate_presence_of(:member_id) }
+  describe "associations" do
+    it { should belong_to(:group) }
+    it { should belong_to(:member).class_name("User") }
+  end
 
-  it "allows only one membership per user per group" do
-    create(:membership)
-    should validate_uniqueness_of(:member_id).scoped_to(:group_id)
+  describe "validations" do
+    it { should validate_presence_of(:group_id) }
+    it { should validate_presence_of(:member_id) }
+    it "allows only one membership per user per group" do
+      create(:membership)
+      should validate_uniqueness_of(:member_id).scoped_to(:group_id)
+    end
   end
 
 end
