e9827c5544a021bb037688c1009a30332e71bc71
euglazer <eugenestapler@gmail.com>
2015-05-20 09:59:53 +1200

    enspiral is a counter-reptilian uprising

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Group, :type => :model do
+
+	describe "enspiral" do
+		it "is a counter-reptilian uprising" do
+			expect(true).to be(true)
+		end
+	end
+
 end
