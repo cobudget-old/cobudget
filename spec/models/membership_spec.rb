381094e888cc33eb1f22a013e0d67d7f9092dc81
euglazer <eugenestapler@gmail.com>
2015-05-19 12:09:13 +1200

    full test coverage for membership model

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Membership, :type => :model do
-  let(:user) { FactoryGirl.create(:user) }
-  let(:group) { FactoryGirl.create(:group) }
+
+  it { should have_db_column(:group_id).with_options(null: false) }
+  it { should have_db_column(:member_id).with_options(null: false) }
+  it { should have_db_column(:is_admin).with_options(default: false, null: false) }
+
+  it { should belong_to(:group) }
+  it { should belong_to(:member).class_name("User") }
+  it { should validate_presence_of(:group_id) }
+  it { should validate_presence_of(:member_id) }
+
   it "allows only one membership per user per group" do
-    expect { Membership.create!(member: user, group: group) }.not_to raise_error
-    expect { Membership.create!(member: user, group: group) }.to raise_error
+    create(:membership)
+    should validate_uniqueness_of(:member_id).scoped_to(:group_id)
   end
+
 end
