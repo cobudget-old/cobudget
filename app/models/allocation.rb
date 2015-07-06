72fbc6d13a5c1bfc59ec4c919da378ec0616fdf8
euglazer <eugenestapler@gmail.com>
2015-07-06 15:23:44 +1200

    add #formatted_amount method to allocation model

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -5,4 +5,9 @@ class Allocation < ActiveRecord::Base
   validates :round_id, presence: true
   validates :user_id, presence: true, uniqueness: {scope: :round_id}
   validates :amount, presence: true
+
+  def formatted_amount
+    Money.new(amount.to_f * 100, "USD").format
+  end
+
 end
