a57586e1eb01dfecb416dfa8f388c9a16bbf4315
euglazer <eugenestapler@gmail.com>
2015-07-08 20:40:25 +1200

    clear sidekiqs enqueued mailers after specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,10 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.after(:all) do 
+    Sidekiq::Queue.new("mailers").clear
+  end
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
