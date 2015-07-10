4f817712283ce1031ff85db2830d0f1f6be0d69d
euglazer <eugenestapler@gmail.com>
2015-07-10 12:11:43 +1200

    removed published attr from round factory

--- spec/factories/rounds.rb
+++ spec/factories/rounds.rb
@@ -3,14 +3,12 @@ FactoryGirl.define do
     name { Faker::Company.name }
     group
     members_can_propose_buckets true
-    published false
   end
 
   factory :draft_round, class: :round do
     name { Faker::Company.name }
     group
     members_can_propose_buckets true
-    published false
   end
 
   factory :round_open_for_proposals, class: :round do
@@ -19,7 +17,6 @@ FactoryGirl.define do
     members_can_propose_buckets true
     starts_at Time.zone.now + 1.days
     ends_at Time.zone.now + 3.days
-    published true
   end
 
   factory :round_open_for_contributions, class: :round do
@@ -28,7 +25,6 @@ FactoryGirl.define do
     members_can_propose_buckets true
     starts_at Time.zone.now - 2.days
     ends_at Time.zone.now + 2.days
-    published true
   end
 
   factory :round_closed, class: :round do
@@ -37,6 +33,5 @@ FactoryGirl.define do
     members_can_propose_buckets true
     starts_at Time.zone.now - 5.days
     ends_at Time.zone.now - 2.days
-    published true
   end
 end
