d5df78b462f4b8166e4e24f61d503ac3fa2254f6
euglazer <eugenestapler@gmail.com>
2015-07-10 12:31:30 +1200

    remove brittle tests, add validator tests

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,106 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Round, :type => :model do
-  describe "fields" do
-    it { should have_db_column(:group_id).with_options(null: false) }
-    it { should have_db_column(:name).of_type(:string).with_options(null: false) }
-    it { should have_db_column(:starts_at).of_type(:datetime) }
-    it { should have_db_column(:ends_at).of_type(:datetime) }
-    it { should have_db_column(:members_can_propose_buckets).of_type(:boolean) }
-    it { should have_db_column(:published).of_type(:boolean).with_options(default: false)}
-  end
-
-  describe "associations" do
-    it { should belong_to(:group) }
-    it { should have_many(:buckets).dependent(:destroy) }
-    it { should have_many(:allocations).dependent(:destroy) }
-    it { should have_many(:fixed_costs).dependent(:destroy) }
-  end
-
-  describe "validations" do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:group) }
-  end
-
-  describe "#generate_new_members_and_allocations_from(csv, current_user)" do
-
-    before do
-      @csv = CSV.read('./spec/assets/test-csv.csv')
-      @group = create(:group)
-      @admin = create(:user)
-      @group.add_admin(@admin)
-      @csv.each { |email, allocation| @group.members << create(:user, email: email) }
-      @round = create(:round, group: @group)
-    end
-
-    it "generates allocations for round from a 2-column csv file containing emails and allocations" do
-      @round.generate_new_members_and_allocations_from!(@csv, @admin)
-      @csv.each do |email, allocation|
-        user_id = User.find_by_email(email).id
-        expect(@round.allocations.find_by(user_id: user_id, amount: allocation.to_i)).to be_truthy
-      end
-    end
-
-    context "if any cobudget users in csv not already a member of group" do
-      before do
-        @new_member = @group.members.last
-        @group.members.delete(@new_member.id)
-        @group.reload
-      end
-
-      it "should add them to the group" do
-        @round.generate_new_members_and_allocations_from!(@csv, @admin)
-        expect(@group.members).to include(@new_member)
-      end
-
-      it "should send them an 'invite to group' email" do
-        mail_double = double('mail')
-        expect(UserMailer).to receive(:invite_to_group_email).with(@new_member, @admin, @group, @round).and_return(mail_double)
-        expect(mail_double).to receive(:deliver_later!)
-        @round.generate_new_members_and_allocations_from!(@csv, @admin)
-      end
-
-      it "should generate allocation for the new group member" do
-        @round.generate_new_members_and_allocations_from!(@csv, @admin)
-        expect(@round.allocations.find_by(user_id: @new_member.id)).to be_truthy
-      end
-    end
-  
-    context "if any users in csv not a part of cobudget at all" do
-      before do
-        @new_member = @group.members.last.delete
-      end
-
-      it "should add them to cobudget" do
-        @round.generate_new_members_and_allocations_from!(@csv, @admin)
-        expect(User.find_by_email(@new_member.email)).to be_truthy
-      end
-
-      it "should add them to the group" do
-        @round.generate_new_members_and_allocations_from!(@csv, @admin)
-        expect(@group.members.find_by_email(@new_member.email)).to be_truthy
-      end
-
-      it "should send them an invite to cobudget email" do
-        mail_double = double('mail')
-        expect(UserMailer).to receive(:invite_to_group_email).and_return(mail_double)
-        expect(mail_double).to receive(:deliver_later!)
-        @round.generate_new_members_and_allocations_from!(@csv, @admin)
-      end
-
-      it "should send them an invite to group email" do
-        mail_double = double('mail')
-        expect(UserMailer).to receive(:invite_email).and_return(mail_double)
-        expect(mail_double).to receive(:deliver_later!)
-        @round.generate_new_members_and_allocations_from!(@csv, @admin)
-      end
-
-      it "should generate allocation for new cobudgeter" do
-        @round.generate_new_members_and_allocations_from!(@csv, @admin)
-        expect(@round.allocations.find_by(user: User.find_by_email(@new_member.email))).to be_truthy
-      end
-    end
-  end
 
   describe "#mode" do
     it "if unpublished -- draft mode" do
@@ -120,157 +20,23 @@ RSpec.describe Round, :type => :model do
     end
   end
 
-  describe "#publish_and_open_for_proposals!(args)" do
-    before do
-      @round = create(:draft_round)
-      @admin = create(:user)
-      @group = @round.group
-      @group.add_admin(@admin)
-      5.times { create(:membership, group: @group) }
-      @members = @group.members
+  describe "#published?" do
+    it "returns true if starts_at and ends_at are both present" do
+      expect(create(:round_open_for_proposals).published?).to eq(true)
     end
 
-    context "if valid arguments" do
-      before do
-        @valid_args = {
-          starts_at: Time.zone.now + 1.days,
-          ends_at: Time.zone.now + 3.days,
-          admin: @admin
-        }
-      end
-
-      it "updates published to true" do
-        @round.publish_and_open_for_proposals!(@valid_args)
-        expect(@round.published).to eq(true)
-      end
-
-      it "immediately sends notification emails to everyone involved in the round" do
-        expect(SendInvitationsToProposeJob).to receive(:perform_later).with(@admin, @group, @round)
-        @round.publish_and_open_for_proposals!(@valid_args)
-      end
-
-      it "schedules 'invite to contribute' emails to be run at the 'starts_at' time specified in params" do
-        configured_job_double = double('configured_job')
-        expect(SendInvitationsToContributeJob).to receive(:set).with(wait_until: @valid_args[:starts_at]).and_return(configured_job_double)
-        expect(configured_job_double).to receive(:perform_later).with(@admin, @group, @round)
-        @round.publish_and_open_for_proposals!(@valid_args)
-      end
-
-      it "schedules 'round is closed' emails to be run at the 'ends_at' time specified in params" do
-        configured_job_double = double('configured_job')
-        expect(SendRoundClosedNotificationsJob).to receive(:set).with(wait_until: @valid_args[:ends_at]).and_return(configured_job_double)
-        expect(configured_job_double).to receive(:perform_later).with(@admin, @group, @round)
-        @round.publish_and_open_for_proposals!(@valid_args)        
-      end
-
-      it "the round's mode is updated to 'proposal'" do
-        @round.publish_and_open_for_proposals!(@valid_args)
-        expect(@round.mode).to eq('proposal')
-      end
-    end
-
-    context "if invalid arguments" do
-      before do
-        @round.publish_and_open_for_proposals!({})
-      end
-
-      it "does not publish the round" do
-        expect(@round.published).to eq(false)
-      end
-
-      it "an error is added to the round" do
-        expect(@round.errors[:starts_at]).to include("and ends_at must both be present, and starts_at must occur before ends_at.")
-      end
+    it "returns false otherwise" do
+      expect(create(:draft_round).published?).to eq(false)
     end
   end
 
-  describe "#publish_and_open_for_contributions!(args)" do
-    before do
-      @round = create(:draft_round)
-      @admin = create(:user)
-      @group = @round.group
-      @group.add_admin(@admin)
-      5.times { create(:membership, group: @group) }
-      @members = @group.members
-      @members.each { |member| create(:allocation, user: member, round: @round) }
+  describe "has_valid_duration?" do
+    it "returns true if starts_at and ends_at are present, and starts_at is before ends_at" do
+      expect(create(:round_open_for_proposals).has_valid_duration?).to eq(true)
     end
 
-    context "if valid arguments" do
-      before do
-        @valid_args = {
-          ends_at: Time.zone.now + 5.days,
-          admin: @admin
-        }
-      end
-
-      it "publishes the round" do
-        @round.publish_and_open_for_contributions!(@valid_args)
-        expect(@round.published).to eq(true)
-      end
-
-      it "updates rounds mode to 'contribution'" do
-        @round.publish_and_open_for_contributions!(@valid_args)
-        expect(@round.mode).to eq('contribution')
-      end
-
-      it "immediately sends emails to members of the round with an allocation, inviting them to contribute" do
-        expect(SendInvitationsToContributeJob).to receive(:perform_later).with(@admin, @group, @round)
-        @round.publish_and_open_for_contributions!(@valid_args)
-      end
-
-      it "schedules 'round is closed' emails to be run at the 'ends_at' time specified in params" do
-        configured_job_double = double('configured_job')
-        expect(SendRoundClosedNotificationsJob).to receive(:set).with(wait_until: @valid_args[:ends_at]).and_return(configured_job_double)
-        expect(configured_job_double).to receive(:perform_later).with(@admin, @group, @round)
-        @round.publish_and_open_for_contributions!(@valid_args)        
-      end
-    end
-
-    context "if invalid arguments" do
-      before do
-        @round.publish_and_open_for_contributions!({})
-      end
-
-      it "does not publish the round" do
-        expect(@round.published).to eq(false)
-      end
-
-      it "adds an error to the round" do
-        expect(@round.errors[:ends_at]).to include('must exist and occur in the future')
-      end
-    end
-  end
-
-  describe "publish!(admin)" do
-    before do
-      @round = create(:round)
-      @admin = create(:user)
-      @round.group.add_admin(@admin)
-    end
-
-    context "if skip_proposals is true" do
-      it "calls publish_and_open_for_contributions!" do
-        args = {
-          skip_proposals: true,
-          admin: @admin,
-          ends_at: Time.zone.now + 4.days
-        }
-        expect(@round).to receive(:publish_and_open_for_contributions!).with(args)
-        @round.publish!(args)
-      end
-    end
-
-    context "if skip_proposals is false" do
-      it "calls publish_and_open_for_proposals!" do
-        args = {
-          skip_proposals: false,
-          admin: @admin,
-          starts_at: Time.zone.now + 1.days,
-          ends_at: Time.zone.now + 4.days
-        }
-        expect(@round).to receive(:publish_and_open_for_proposals!).with(args)
-        @round.publish!(args)
-      end
+    it "otherwise, returns false" do
+      expect(create(:draft_round).has_valid_duration?).to eq(false)
     end
   end
 
@@ -287,5 +53,18 @@ RSpec.describe Round, :type => :model do
     it 'validates that starts_at is before ends_at' do
       expect { Round.create!(name: 'hi', group: group, starts_at: Time.now + 2.days, ends_at: Time.now + 1.day) }.to raise_error
     end    
+  end
+
+  describe "#has_valid_duration_validator" do
+    it "checks that a round has a valid_duration" do
+      round = build(:round_open_for_proposals)
+      expect(round.valid?).to eq(true)
+    end
+
+    it "adds an error if round duration not valid" do
+      round = build(:draft_round)
+      round.assign_attributes(starts_at: Time.zone.now, ends_at: Time.zone.now - 1.days)
+      expect(round.valid?).to eq(false)
+    end
   end
 end
