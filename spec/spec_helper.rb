33ea909d6b2a229165d29ae421e8b8ee764beb9d
euglazer <eugenestapler@gmail.com>
2015-07-15 11:03:07 +1200

    remove helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,6 @@ RSpec.configure do |config|
   config.after(:all) do 
     Sidekiq::Queue.new("mailers").clear
     Sidekiq::ScheduledSet.new.each { |job| job.delete }
-    reset_mailer
   end
 
 # The settings below are suggested to provide a good initial experience
