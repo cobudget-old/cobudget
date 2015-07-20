0143a6ed64714baed67ec24a297100f6914deff6
euglazer <eugenestapler@gmail.com>
2015-07-21 11:40:26 +1200

    group has many allocations and buckets

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
-  has_many :rounds, -> { order("ends_at DESC") }, dependent: :destroy
-  has_one  :latest_round, -> { order("id DESC") }, class_name: 'Round'
+  has_many :buckets, dependent: :destroy
+  has_many :allocations, dependent: :destroy
   has_many :memberships
   has_many :members, through: :memberships, source: :member
 
