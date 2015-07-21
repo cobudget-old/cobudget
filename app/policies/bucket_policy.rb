b5cd70b2c5c9899e25f2b8b6cb69fb9e9b9e4642
euglazer <eugenestapler@gmail.com>
2015-07-21 17:20:37 +1200

    add new bucket policies

--- app/policies/bucket_policy.rb
+++ app/policies/bucket_policy.rb
@@ -1,21 +1,13 @@
 class BucketPolicy < ApplicationPolicy
   def create?
-    (
-      user.is_admin_for?(record.round.group) and
-      record.round.mode != 'closed'
-    ) or (
-      record.round.group.members.include?(user) and
-      (record.user_id == user.id) and
-      record.round.members_can_propose_buckets and
-      record.round.mode == "proposal"
-    )
+    user.is_member_of?(record.group)
   end
 
   def update?
-    create?
+    user.is_member_of?(record.group) && (record.user == user || user.is_admin_for?(record.group))
   end
 
   def destroy?
-    create?
+    user.is_member_of?(record.group) && (record.user == user || user.is_admin_for?(record.group))
   end
 end
