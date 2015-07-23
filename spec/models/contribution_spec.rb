acd0ea55c6b0bafddbbc0b51aa185130aa0c587e
euglazer <eugenestapler@gmail.com>
2015-07-23 14:59:55 +1200

    remove irrelevant contribution model specs

--- spec/models/contribution_spec.rb
+++ spec/models/contribution_spec.rb
@@ -1,18 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Contribution, :type => :model do
-  describe "#self.for_round(round_id)" do
-  	it "returns all contributions from buckets with specified round_id" do
-  		round1 = create(:round_open_for_contributions)
-  		bucket1 = create(:bucket, round: round1)	
-  		contributions1 = Array.new(5) { create(:contribution, bucket: bucket1) }
-
-  		round2 = create(:round_open_for_contributions)
-  		bucket2 = create(:bucket, round: round2)	
-  		contributions2 = Array.new(5) { create(:contribution, bucket: bucket2) }
-
-  		expect(Contribution.for_round(round1.id)).to eq(contributions1)
-  		expect(Contribution.for_round(round2.id)).to eq(contributions2)
-  	end
-  end
 end
