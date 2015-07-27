d18f1d68f67ed5910b9e5074ec86ef03d94fa219
Michael Williams <dinosaur@riseup.net>
2015-07-27 16:55:49 +1200

    set default config to development

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,7 @@
 var assign = require('lodash').assign
 
 var defaults = require('../../config/defaults')
-var config = require('../../config/' + process.env.NODE_ENV)
+var config = require('../../config/' + (process.env.NODE_ENV || 'development'))
 
 module.exports = assign({}, defaults, config, {
   env: process.env.NODE_ENV,
