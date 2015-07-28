6ccf071866698924dec938c9036a9825e3f6db7f
euglazer <eugenestapler@gmail.com>
2015-07-28 20:11:34 +1200

    namespace api routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,30 +3,35 @@ Rails.application.routes.draw do
     password: 'reset_password'
   }
   apipie
+
+  scope path: "/api/v1", defaults: { format: :json } do
+    resources :users, only: [:index, :update], defaults: { format: :json } do
+      member do
+        post :change_password
+      end
+    end
+
+    resources :groups, only: [:index, :show, :create], defaults: { format: :json } do
+      resources :allocations, only: [:index]
+      resources :memberships, only: [:index]
+    end
+    resources :memberships, only: [:create, :update, :destroy], defaults: { format: :json }
+
+    # NOTE (JL): Added unsed show route here cause otherwise respond_with doesn't work
+    # on the create action (not sure why??)
+    resources :allocations, only: [:create, :show, :update], defaults: { format: :json }
+
+    resources :buckets, only: [:create, :show, :update, :destroy], defaults: { format: :json }
+
+    resources :contributions, only: [:create, :update, :destroy], defaults: { format: :json }
+
+    root to: redirect('/docs')
+  end
+
   resources :auth, only: [], defaults: { format: :json } do
     collection do
       post :log_in
     end
   end
-  resources :users, only: [:index, :update], defaults: { format: :json } do
-    member do
-      post :change_password
-    end
-  end
-
-  resources :groups, only: [:index, :show, :create], defaults: { format: :json } do
-    resources :allocations, only: [:index]
-    resources :memberships, only: [:index]
-  end
-  resources :memberships, only: [:create, :update, :destroy], defaults: { format: :json }
-
-  # NOTE (JL): Added unsed show route here cause otherwise respond_with doesn't work
-  # on the create action (not sure why??)
-  resources :allocations, only: [:create, :show, :update], defaults: { format: :json }
-
-  resources :buckets, only: [:create, :show, :update, :destroy], defaults: { format: :json }
-
-  resources :contributions, only: [:create, :update, :destroy], defaults: { format: :json }
-
-  root to: redirect('/docs')
+  
 end
