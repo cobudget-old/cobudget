057f257c2da3fdf78490ed165233f99bb8de4602
euglazer <eugenestapler@gmail.com>
2015-07-29 15:39:20 +1200

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723024055) do
+ActiveRecord::Schema.define(version: 20150729023949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,9 @@ ActiveRecord::Schema.define(version: 20150723024055) do
     t.string   "access_token"
     t.string   "name"
     t.boolean  "initialized",            default: false, null: false
+    t.text     "tokens"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["access_token"], name: "index_users_on_access_token", unique: true, using: :btree
