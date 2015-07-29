778425f0306a829194cdb1a87ee136f2c59ca81e
euglazer <eugenestapler@gmail.com>
2015-07-29 19:31:55 +1200

    namespace routes again

--- config/routes.rb
+++ config/routes.rb
@@ -4,32 +4,29 @@ Rails.application.routes.draw do
     password: 'reset_password'
   }
   apipie
-  resources :auth, only: [], defaults: { format: :json } do
-    collection do
-      post :log_in
-    end
-  end
-  
+
   resources :users, only: [:index, :update], defaults: { format: :json } do
     member do
       post :change_password
     end
   end
 
-  resources :groups, only: [:index, :show, :create], defaults: { format: :json } do
-    resources :allocations, only: [:index]
-    resources :memberships, only: [:index]
+  scope path: 'api/v1', defaults: { format: :json } do
+    resources :groups, only: [:index, :show, :create] do
+      resources :allocations, only: [:index]
+      resources :memberships, only: [:index]
+    end
+    resources :memberships, only: [:create, :update, :destroy]
+
+    # NOTE (JL): Added unsed show route here cause otherwise respond_with doesn't work
+    # on the create action (not sure why??)
+    resources :allocations, only: [:create, :show, :update]
+
+    resources :buckets, only: [:create, :show, :update, :destroy]
+
+    resources :contributions, only: [:create, :update, :destroy]
   end
-  resources :memberships, only: [:create, :update, :destroy], defaults: { format: :json }
-
-  # NOTE (JL): Added unsed show route here cause otherwise respond_with doesn't work
-  # on the create action (not sure why??)
-  resources :allocations, only: [:create, :show, :update], defaults: { format: :json }
-
-  resources :buckets, only: [:create, :show, :update, :destroy], defaults: { format: :json }
-
-  resources :contributions, only: [:create, :update, :destroy], defaults: { format: :json }
-
-  root to: redirect('/docs')
   
+  root to: redirect('/docs')
+
 end
