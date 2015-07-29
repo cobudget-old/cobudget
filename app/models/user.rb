6469bbc12afa0575e67c513c3d0aa2781f92f65a
euglazer <eugenestapler@gmail.com>
2015-07-29 15:40:47 +1200

    configure user model for devise_token_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  include TokenAuthenticable
+  include DeviseTokenAuth::Concerns::User
+
+  before_validation :assign_uid_and_provider
+
+
+  ### from previous authentication scheme ###
+  # include TokenAuthenticable
 
   has_many :allocations
 
@@ -21,4 +26,10 @@ class User < ActiveRecord::Base
   def is_member_of?(group)
     group.members.include?(self)
   end
+
+  private
+    def assign_uid_and_provider
+      self.uid = self.email
+      self.provider = "email"
+    end
 end
