5857f5a3a185d5be775dae8e9aad6ec703b28bd7
euglazer <eugenestapler@gmail.com>
2015-07-30 14:02:20 +1200

    clean up application controller

--- app/controllers/application-controller.js
+++ app/controllers/application-controller.js
@@ -1,32 +1,4 @@
 /* @ngInject */
-global.cobudgetApp.controller('ApplicationController', function ($scope, $modal, $urlRouter, authModel, $http, login) {
+global.cobudgetApp.controller('ApplicationController', function ($scope, $modal, $urlRouter, $http, $q) {
 
-  $scope.currentUser = authModel;
-
-  $scope.updateCurrentUser = function () {
-    $scope.updateCurrentUserHeaders();
-  };
-
-  $scope.updateCurrentUserHeaders = function () {
-    var currentUser = $scope.currentUser;
-    var defaultHeaders = {
-      "Accept": "application/json",
-      "X-User-Token": currentUser.accessToken,
-      "X-User-Email": currentUser.email,
-    };
-    $http.defaults.headers.common = defaultHeaders;
-  };
-
-  authModel.on("login-success", function () {
-    $scope.updateCurrentUser();
-    $urlRouter.sync();
-  });
-
-  authModel.on("logout-success", function () {
-    $scope.updateCurrentUser();
-  });
-
-  $scope.updateCurrentUser();
-
-  login.openLogin()
 });
