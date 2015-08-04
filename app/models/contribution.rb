69725f8f51f4bef680bc89a45ef219206d8dbcb7
euglazer <eugenestapler@gmail.com>
2015-08-04 20:16:53 +1200

    add callback contribution#reset_amount_if_exceeds_target

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -5,4 +5,13 @@ class Contribution < ActiveRecord::Base
   validates :bucket_id, presence: true
   validates :user_id, presence: true
   validates :amount, numericality: { greater_than: 0 }
+
+  before_save :reset_amount_if_exceeds_target
+
+  private 
+    def reset_amount_if_exceeds_target
+      if bucket.total_contributions + self.amount > bucket.target
+        self.amount = bucket.target - bucket.total_contributions
+      end
+    end
 end
--- spec/models/contribution_spec.rb
+++ spec/models/contribution_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Contribution, :type => :model do
+  describe "#reset_amount_if_exceeds_target" do
+    it "changes amount to exactly reach target, if initial amount exceeds it" do
+      bucket = create(:bucket, group: group, target: 500)
+      make_user_group_member
+      create(:contribution, amount: 200, user: user, bucket: bucket)
+      contribution = create(:contribution, amount: 600, user: user, bucket: bucket)
+
+      bucket.reload
+      contribution.reload
+
+      expect(bucket.total_contributions).to eq(500)
+      expect(contribution.amount).to eq(300)
+    end
+  end
 end
