4fb93cc4adc160e7683e8f6df6589f3df34ca8be
euglazer <eugenestapler@gmail.com>
2015-08-04 22:18:11 +1200

    add concept of drafts to buckets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730025709) do
+ActiveRecord::Schema.define(version: 20150804094316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150730025709) do
     t.integer  "user_id"
     t.decimal  "target",      precision: 12, scale: 2, default: 0.0
     t.integer  "group_id"
+    t.boolean  "published",                            default: false
   end
 
   add_index "buckets", ["user_id"], name: "index_buckets_on_user_id", using: :btree
--- spec/factories/buckets.rb
+++ spec/factories/buckets.rb
@@ -4,5 +4,13 @@ FactoryGirl.define do
     group
     user
     target 500
+    published true
+  end
+
+  factory :draft_bucket, class: Bucket do
+    name { Faker::Company.name }
+    group
+    user
+    published false
   end
 end
