c90c2292b79c12b5f8b32565c6bdc5b49ed15864
euglazer <eugenestapler@gmail.com>
2015-08-04 22:18:26 +1200

    add data to serializer

--- app/serializers/bucket_serializer.rb
+++ app/serializers/bucket_serializer.rb
@@ -2,9 +2,11 @@ class BucketSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id,
              :group_id,
+             :user_id,
              :name,
              :target,
              :description,
              :created_at,
+             :published,
              :total_contributions
 end
