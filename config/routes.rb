40fa9fe202bfc8151a9fe7bafbc98a58cef69e26
euglazer <eugenestapler@gmail.com>
2015-08-12 17:35:18 +1200

    rename routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,28 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-  devise_for :users, defaults: {format: :json}, skip: [:sessions, :registrations], path_names: {
-    password: 'reset_password'
-  }
   apipie
 
-  resources :users, only: [:index, :update], defaults: { format: :json } do
-    member do
-      post :change_password
-    end
-  end
 
   scope path: 'api/v1', defaults: { format: :json } do
-    resources :groups, only: [:index, :show, :create] do
-      resources :allocations, only: [:index]
-      resources :memberships, only: [:index]
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+    devise_for :users, defaults: {format: :json}, skip: [:sessions, :registrations], path_names: {
+      password: 'reset_password'
+    }
+
+    resources :users, only: [:update], defaults: { format: :json } do
+      member do
+        post :change_password
+      end
     end
-    resources :memberships, only: [:create, :update, :destroy]
+
+    resources :allocations, only: [:index]
+
+    resources :groups, only: [:index, :show, :create]
+
+    resources :memberships, only: [:index, :create, :update, :destroy] do
+      collection do
+        get :my_memberships
+      end
+    end
 
     # NOTE (JL): Added unsed show route here cause otherwise respond_with doesn't work
     # on the create action (not sure why??)
