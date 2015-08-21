1fc607177d51320c41aa33ad5aedb6207b6ccf9f
euglazer <eugenestapler@gmail.com>
2015-08-22 10:31:34 +1200

    allow :patch requests in application.rb

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module CobudgetApi
         resource '*',
           :headers => :any,
           :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          :methods => [:get, :post, :options, :delete, :put]
+          :methods => [:get, :post, :options, :delete, :put, :patch]
       end
     end
 
