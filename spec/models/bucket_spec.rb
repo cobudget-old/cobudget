815e18656a914debec560eaf23d3f8b07d169d70
euglazer <eugenestapler@gmail.com>
2015-08-26 23:57:17 +1200

    Bucket#total_contributions spec - add definite target

--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Bucket, :type => :model do
   describe "#total_contributions" do
     it "returns the sum of contribution amounts for bucket" do
+      bucket = create(:bucket, target: 1000)
       create(:contribution, bucket: bucket, amount: 100)
       create(:contribution, bucket: bucket, amount: 220)
       create(:contribution, bucket: bucket, amount: 40)
