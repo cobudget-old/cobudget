b94125a820c206087404c147a65c99ab61a116d6
euglazer <eugenestapler@gmail.com>
2015-09-13 12:48:21 -0700

    add currency attributes to group serializer

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,4 +1,4 @@
 class GroupSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :name, :balance
+  attributes :id, :name, :balance, :currency_symbol, :currency_code
 end
