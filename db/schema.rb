54772a65816749902bc867d1e96b3ee2b8a5bde3
euglazer <eugenestapler@gmail.com>
2015-09-23 12:34:39 -0700

    add confirmation_token to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912210409) do
+ActiveRecord::Schema.define(version: 20150923193236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 20150912210409) do
     t.text     "tokens"
     t.string   "provider"
     t.string   "uid"
+    t.string   "confirmation_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
