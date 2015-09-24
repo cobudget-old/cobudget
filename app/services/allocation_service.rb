8662d1b8c43debd056333d031f1b420cae749302
euglazer <eugenestapler@gmail.com>
2015-09-23 18:47:17 -0700

    fix logic in AllocationService#create_allocations_from_csv

--- app/services/allocation_service.rb
+++ app/services/allocation_service.rb
@@ -2,17 +2,15 @@ class AllocationService
   def self.create_allocations_from_csv(csv: , group: , current_user:)
     csv.each do |email, amount|
       email = email.downcase.strip
+      user = User.find_by_email(email)
 
-      unless user = User.find_by_email(email)
-        user = User.create_with_confirmation_token(email: email)
-        if user.valid?
-          UserMailer.invite_email(user: user, group: group, inviter: current_user).deliver_later
-        end
-      end
-
-      unless group.members.find_by_id(user.id)
+      if user && !group.members.find_by_id(user.id)
         group.members << user
         UserMailer.invite_to_group_email(user: user, inviter: current_user, group: group).deliver_later
+      elsif !user
+        user = User.create_with_confirmation_token(email: email)
+        group.members << user
+        UserMailer.invite_email(user: user, group: group, inviter: current_user).deliver_later if user.valid?
       end
 
       Allocation.create(group: group, user: user, amount: amount)
