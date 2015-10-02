eb235f09359de64f62b061e0bd0bc67f00d72227
euglazer <eugenestapler@gmail.com>
2015-10-01 18:05:43 -0700

    allocation must be non-zero

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -5,6 +5,7 @@ class Allocation < ActiveRecord::Base
   validates :group_id, presence: true
   validates :user_id, presence: true
   validates :amount, presence: true
+  validates_numericality_of :amount, other_than: 0
 
   def formatted_amount
     Money.new(amount.to_f * 100, currency_code).format
