39e2f0405944ea97f6a0ca4943e4bf720ea03be6
euglazer <eugenestapler@gmail.com>
2015-10-02 16:36:48 -0700

    skip authentication before confirm_account action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < AuthenticatedController
 
+  skip_before_action :authenticate_user!, only: :confirm_account
+
   api :POST, '/users/confirm_account'
   def confirm_account
     if user = User.find_by_confirmation_token(params[:confirmation_token])
