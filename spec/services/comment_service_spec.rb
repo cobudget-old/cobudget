18db74f5c9a4f0f636779432a1b227e78537fd3b
euglazer <eugenestapler@gmail.com>
2015-10-27 10:40:59 -0700

    add comment_service spec for later

---
+++ spec/services/comment_service_spec.rb
@@ -0,0 +1,14 @@
+xdescribe "CommentService" do
+  # waiting to hear from derek before writing these tests
+
+  describe "#send_new_comment_emails(comment: )" do
+
+    context "bucket's author has been deleted" do
+
+    end
+
+    context "commentor is also author of bucket" do
+
+    end
+  end
+end
