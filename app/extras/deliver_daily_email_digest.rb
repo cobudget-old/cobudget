98369e8c0825fac1995b02bcb60b0f80ef2a183b
euglazer <eugenestapler@gmail.com>
2015-11-06 12:43:42 -0800

    add todo for later

--- app/extras/deliver_daily_email_digest.rb
+++ app/extras/deliver_daily_email_digest.rb
@@ -1,4 +1,6 @@
 class DeliverDailyEmailDigest
+  # TODO LATER: scope this to users who have actually subscribed to the digest
+  # until the email settings feature is complete, email digest will be default
   def self.to_subscribers!
     unique_utc_offsets = User.pluck('DISTINCT utc_offset').select do |utc_offset|
       (Time.now.utc + utc_offset.minutes).hour == 6 if utc_offset
