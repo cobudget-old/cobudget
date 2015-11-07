a7f4e759c5d8692a047e9b8de6661c0f66733a8b
euglazer <eugenestapler@gmail.com>
2015-11-06 20:03:45 -0800

    author receives comment notifications on their buckets if subscribed to personal activity

--- app/services/comment_service.rb
+++ app/services/comment_service.rb
@@ -5,7 +5,7 @@ class CommentService
     comment_author = comment.user
 
     # in case bucket_author was deleted
-    if bucket_author && bucket_author != comment_author
+    if bucket_author && bucket_author != comment_author && bucket_author.subscribed_to_personal_activity
       UserMailer.notify_author_of_new_comment_email(comment: comment).deliver_later
     end
 
--- spec/services/comment_service_spec.rb
+++ spec/services/comment_service_spec.rb
@@ -1,14 +1,33 @@
-xdescribe "CommentService" do
-  # waiting to hear from derek before writing these tests
+require "rails_helper"
 
+describe "CommentService" do
   describe "#send_new_comment_emails(comment: )" do
-
-    context "bucket's author has been deleted" do
-
+    before do
+      @bucket_author = bucket.user
+      @comment = create(:comment, bucket: bucket)
+      @comment_author = @comment.user
     end
 
-    context "commentor is also author of bucket" do
+    after do 
+      ActionMailer::Base.deliveries.clear
+    end
 
+    context "bucket author subscribed to personal activity" do
+      it "bucket author receives notification email" do
+        @bucket_author.update(subscribed_to_personal_activity: true)
+        CommentService.send_new_comment_emails(comment: @comment)
+        email_recipients = ActionMailer::Base.deliveries.map { |e| e.to.first }
+        expect(email_recipients).to include(@bucket_author.email)
+      end
+    end
+
+    context "bucket author not subscribed to personal activity" do
+      it "bucket author does not receive notification email" do
+        @bucket_author.update(subscribed_to_personal_activity: false)
+        CommentService.send_new_comment_emails(comment: @comment)
+        email_recipients = ActionMailer::Base.deliveries.map { |e| e.to.first }
+        expect(email_recipients).not_to include(@bucket_author.email)
+      end
     end
   end
 end
