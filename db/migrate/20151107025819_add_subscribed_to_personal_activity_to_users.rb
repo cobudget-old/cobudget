f7ee6b45fc0f385b9748ee23cd61af2e71804985
euglazer <eugenestapler@gmail.com>
2015-11-06 19:46:11 -0800

    typo

--- db/migrate/20151107025819_add_subscribed_to_personal_activity_to_users.rb
+++ db/migrate/20151107025819_add_subscribed_to_personal_activity_to_users.rb
@@ -1,10 +1,10 @@
 class AddSubscribedToPersonalActivityToUsers < ActiveRecord::Migration
   def up
-    add_column :users, :add_subscribed_to_personal_activity, :boolean, default: true
-    User.all.each { |u| u.update(add_subscribed_to_personal_activity: true) }
+    add_column :users, :subscribed_to_personal_activity, :boolean, default: true
+    User.all.each { |u| u.update(subscribed_to_personal_activity: true) }
   end
 
   def down
-    remove_column :users, :add_subscribed_to_personal_activity    
+    remove_column :users, :subscribed_to_personal_activity    
   end
 end
