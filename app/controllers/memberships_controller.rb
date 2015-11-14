70b54efef3d84976de07170b7a32a5608315e618
euglazer <eugenestapler@gmail.com>
2015-11-13 16:11:09 -0800

    add active scopes to get methods in memberships_controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -2,12 +2,12 @@ class MembershipsController < AuthenticatedController
   api :GET, 'memberships?group_id=', 'Get memberships for a particular group'
   def index
     group = Group.find(params[:group_id])
-    render json: group.memberships
+    render json: group.memberships.active
   end
 
   api :GET, 'memberships/my_memberships', 'Get memberships for the current_user'
   def my_memberships
-    render json: Membership.where(member_id: current_user.id)
+    render json: Membership.where(member_id: current_user.id).active
   end
 
   def update
