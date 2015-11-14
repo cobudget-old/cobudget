3ea22c552f0f749525a3dfcb51a535cf3f6bda48
euglazer <eugenestapler@gmail.com>
2015-11-13 22:05:46 -0800

    fix params on BucketService.send_bucket_created_emails call in Buckets#create

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -15,7 +15,7 @@ class BucketsController < AuthenticatedController
   def create
     bucket = Bucket.new(bucket_params_create)
     if bucket.save
-      # BucketService.send_bucket_created_emails(bucket: bucket, current_user: current_user)
+      # BucketService.send_bucket_created_emails(bucket: bucket)
       render json: [bucket]
     else 
       render json: {
