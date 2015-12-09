497b14f932c25daf7fbf99f4e296a52d669f35b3
euglazer <eugenestapler@gmail.com>
2015-12-09 15:52:28 -0800

    add is_pending_confirmation property to user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, 
-             :name, 
-             :email, 
-             :utc_offset, 
+  attributes :id,
+             :name,
+             :email,
+             :utc_offset,
              :subscribed_to_personal_activity,
              :subscribed_to_daily_digest,
-             :subscribed_to_participant_activity
+             :subscribed_to_participant_activity,
+             :is_pending_confirmation
+
+  def is_pending_confirmation
+    !object.has_set_up_account?
+  end
 end
