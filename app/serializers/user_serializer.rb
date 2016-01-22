df6358ad4186185e72f74aca2a917ff157a4b5a4
euglazer <eugenestapler@gmail.com>
2016-01-22 17:27:58 +1300

    add joined_first_group_at to user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -7,5 +7,6 @@ class UserSerializer < ActiveModel::Serializer
              :subscribed_to_personal_activity,
              :subscribed_to_daily_digest,
              :subscribed_to_participant_activity,
-             :confirmed_at
+             :confirmed_at,
+             :joined_first_group_at
 end
