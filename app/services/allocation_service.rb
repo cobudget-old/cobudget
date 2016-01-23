d031ac9e65ed61792d950ff1e22c1e6d75843be7
euglazer <eugenestapler@gmail.com>
2016-01-23 13:20:00 +1300

    small refactor

--- app/services/allocation_service.rb
+++ app/services/allocation_service.rb
@@ -8,12 +8,12 @@ class AllocationService
         if user.is_member_of?(group)
           UserMailer.notify_member_that_they_received_allocation(admin: current_user, member: user, group: group, amount: amount).deliver_later if amount > 0
         else
-          group.members << user
+          group.add_member(user)
           UserMailer.invite_to_group_email(user: user, inviter: current_user, group: group, initial_allocation_amount: amount).deliver_later
         end
       else
         user = User.create_with_confirmation_token(email: email)
-        group.members << user
+        group.add_member(user)
         UserMailer.invite_email(user: user, group: group, inviter: current_user, initial_allocation_amount: amount).deliver_later if user.valid?
       end
 
