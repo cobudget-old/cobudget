b284f268dbb198a3f2c00154ad59d65fe038fbb8
euglazer <eugenestapler@gmail.com>
2016-02-26 01:27:52 -0800

    remove old application controller stuff from alpha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,41 +5,5 @@ class ApplicationController < ActionController::Base
   include ActionController::ImplicitRender
   include ActionController::Serialization
 
-  include Pundit
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   respond_to :json
-
-private
-  def user_not_authorized
-    render json: { errors: { user: ["not authorized to do this"] } }, status: 403
-  end
-
-  # CRUD helpers
-  #
-
-  def resource
-    @resource ||= controller_name.classify.constantize.find(params[:id])
-  end
-
-  def create_resource(controller_params, **args)
-    resource = controller_name.classify.constantize.new(controller_params)
-    authorize resource
-    resource.save
-    if args[:respond] == false
-      resource
-    else
-      respond_with resource
-    end
-  end
-
-  def update_resource(controller_params)
-    authorize resource
-    respond_with resource.update_attributes(controller_params)
-  end
-
-  def destroy_resource
-    authorize resource
-    respond_with resource.destroy
-  end
 end
