b84c347b0b8a0c6120debeee0f4b2bfd3c74f60a
euglazer <eugenestapler@gmail.com>
2016-02-26 01:26:21 -0800

    ApplicationController inherits from ActionController::Base so that respond_to method can be used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::ImplicitRender
   include ActionController::Serialization
-  
+
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
