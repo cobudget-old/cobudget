aa06217f91cf49d91ac989ea85cebaef5bcf0957
euglazer <eugenestapler@gmail.com>
2016-03-10 14:29:54 -0800

    add validation to contribution model -- amount cannot overdraft member

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -5,6 +5,7 @@ class Contribution < ActiveRecord::Base
   validates :bucket_id, presence: true
   validates :user_id, presence: true
   validates :amount, numericality: { greater_than: 0 }
+  validate :amount_cannot_overdraft_member
 
   before_save :lower_amount_if_exceeds_target
   after_save :update_bucket_status_if_funded
@@ -13,7 +14,7 @@ class Contribution < ActiveRecord::Base
     Money.new(amount * 100, currency_code).format
   end
 
-  private 
+  private
     def lower_amount_if_exceeds_target
       if bucket.total_contributions + self.amount > bucket.target
         self.amount = bucket.target - bucket.total_contributions
@@ -23,6 +24,13 @@ class Contribution < ActiveRecord::Base
     def update_bucket_status_if_funded
       if bucket.status == 'live' && bucket.funded?
         bucket.update(status: 'funded')
+      end
+    end
+
+    def amount_cannot_overdraft_member
+      membership = Membership.find_by(member_id: user_id, group_id: bucket.group_id)
+      if membership.raw_balance + amount < 0
+        errors.add(:amount, "amount cannot overdraft member")
       end
     end
 
--- spec/factories/contributions.rb
+++ spec/factories/contributions.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     user
     bucket
     amount 1
+
+    before(:create) do |contribution|
+      Membership.find_or_create_by(member: contribution.user, group: contribution.bucket.group)
+    end
   end
 end
