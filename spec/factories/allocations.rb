79a571f863f54fae6ae44f26a9fe2057914a26f6
euglazer <eugenestapler@gmail.com>
2016-03-10 14:22:57 -0800

    add validation to allocation model -- amount cannot overdraft member

--- spec/factories/allocations.rb
+++ spec/factories/allocations.rb
@@ -3,5 +3,8 @@ FactoryGirl.define do
     user
     group
     amount 40
+    before(:create) do |allocation|
+      Membership.find_or_create_by(member: allocation.user, group: allocation.group)
+    end
   end
 end
