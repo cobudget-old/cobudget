334aadd95dc32a0342bc018103e537676faf3bfb
euglazer <eugenestapler@gmail.com>
2016-03-11 13:34:05 -0800

    remove old subscription properties from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311100322) do
+ActiveRecord::Schema.define(version: 20160311213104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,12 +121,12 @@ ActiveRecord::Schema.define(version: 20160311100322) do
   add_index "subscription_trackers", ["user_id"], name: "index_subscription_trackers_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                              default: "",    null: false
-    t.string   "encrypted_password",                 default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -139,9 +139,6 @@ ActiveRecord::Schema.define(version: 20160311100322) do
     t.string   "uid"
     t.string   "confirmation_token"
     t.integer  "utc_offset"
-    t.boolean  "subscribed_to_personal_activity",    default: false
-    t.boolean  "subscribed_to_daily_digest",         default: false
-    t.boolean  "subscribed_to_participant_activity", default: false
     t.datetime "confirmed_at"
     t.datetime "joined_first_group_at"
   end
