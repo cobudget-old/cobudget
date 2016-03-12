35d37da9fa0f5a69f5bd733932490ff1081c75df
euglazer <eugenestapler@gmail.com>
2016-03-12 00:07:09 -0800

    outline UserService#fetch_recent_activity_for(user:) and specs

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,26 +1,81 @@
 class UserService
-  def self.fetch_recent_activity_for(user:)
-    return nil unless user.utc_offset
-    user_utc_offset_in_hours = user.utc_offset / 60
-    user_6am_today_in_utc = (DateTime.now.in_time_zone(user_utc_offset_in_hours).beginning_of_day + 6.hours).utc 
-    user_6am_yesterday_in_utc = user_6am_today_in_utc - 1.day
+  attr_reader :user
 
-    recent_activity = user.memberships.active.map do |membership|
-      group = membership.group
-      draft_buckets = Bucket.where(group: group, status: 'draft', created_at: (user_6am_yesterday_in_utc ... user_6am_today_in_utc ))
-      live_buckets = Bucket.where(group: group, status: 'live', live_at: (user_6am_yesterday_in_utc ... user_6am_today_in_utc ))
-      funded_buckets = Bucket.where(group: group, status: 'funded', funded_at: (user_6am_yesterday_in_utc ... user_6am_today_in_utc ))
-      {
-        group: group,
-        membership: membership,
-        draft_buckets: draft_buckets,
-        live_buckets: live_buckets,
-        funded_buckets: funded_buckets
-      }
+  def self.fetch_recent_activity_for(user:)
+    @@user = user
+    Hash[users_active_groups.map { |group| collated_activity_for_group(group) }]
+  end
+
+  def self.collated_activity_for_group(group)
+    [group, {
+      comments_on_buckets_user_participated_in: comments_on_buckets_user_participated_in.joins(:bucket).where(bucket: {group: group}),
+      comments_on_users_buckets: comments_on_users_buckets.joins(:bucket).where(bucket: {group: group}),
+      contributions_to_users_buckets: contributions_to_users_buckets.joins(:bucket).where(bucket: {group: group}),
+      contributions_to_buckets_user_participated_in: contributions_to_buckets_user_participated_in.joins(:bucket).where(bucket: {group: group}),
+      users_buckets_fully_funded: users_buckets_fully_funded.where(group: group),
+      new_draft_buckets: new_draft_buckets.where(group: group),
+      new_live_buckets: new_live_buckets.where(group: group),
+      new_funded_buckets: new_funded_buckets.where(group: group)
+    }]
+  end
+
+  def self.comments_on_buckets_user_participated_in
+    @@comments_on_buckets_user_participated_in ||= Comment.where(bucket: buckets_user_participated_in, created_at: time_range)
+  end
+
+  def self.comments_on_users_buckets
+    @@comments_on_users_buckets ||= Comment.where(bucket: user_buckets, created_at: time_range)
+  end
+
+  def self.contributions_to_users_buckets
+    @@contributions_to_users_buckets ||= Contribution.where(bucket: user_buckets, created_at: time_range)
+  end
+
+  def self.contributions_to_buckets_user_participated_in
+    @@contributions_to_buckets_user_participated_in ||= Contribution.where(bucket: buckets_user_participated_in, created_at: time_range)
+  end
+
+  def self.users_buckets_fully_funded
+    @@users_buckets_fully_funded ||= user_buckets.where(status: 'funded', funded_at: time_range)
+  end
+
+  def self.new_draft_buckets
+    @@new_draft_buckets ||= user_group_buckets.where(status: 'draft', created_at: time_range)
+  end
+
+  def self.new_live_buckets
+    @@new_live_buckets ||= user_group_buckets.where(status: 'live', live_at: time_range)
+  end
+
+  def self.new_funded_buckets
+    @@new_funded_buckets ||= user_group_buckets.where(status: 'funded', funded_at: time_range)
+  end
+
+  private
+    def self.subscription_tracker
+      @@subscription_tracker ||= user.subscription_tracker
     end
 
-    recent_activity.select! { |a| a[:draft_buckets].any? || a[:live_buckets].any? || a[:funded_buckets].any? }
+    def self.time_range
+      @@time_range ||= (subscription_tracker.recent_activity_last_fetched_at..subscription_tracker.next_recent_activity_fetch_scheduled_at)
+    end
 
-    recent_activity if recent_activity.any?
-  end
+    def self.buckets_user_participated_in
+      @@buckets_user_participated_in ||= Bucket.joins(:comments, :contributions).where(
+        comments: {user: user},
+        contributions: {user: user}
+      )
+    end
+
+    def self.user_buckets
+      @@user_buckets ||= user.buckets
+    end
+
+    def self.user_group_buckets
+      @@user_group_buckets ||= Bucket.where(group: users_active_groups)
+    end
+
+    def self.users_active_groups
+      @@users_active_groups ||= Group.joins(:memberships).where(memberships: {member: user, archived_at: nil})
+    end
 end
--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -1,149 +1,182 @@
 require 'rails_helper'
 
 describe "UserService" do
-  describe "#fetch_recent_activity_for" do
-    context "user has one membership in a group with recent activity" do
-      before do
-        @membership = make_user_group_member
-        user.update(utc_offset: -480) # user is in oakland
-        utc_offset_in_hours = user.utc_offset / 60
-        oakland_6am_today_in_utc = (DateTime.now.in_time_zone(utc_offset_in_hours).beginning_of_day + 6.hours).utc
-        oakland_6am_yesterday_in_utc = oakland_6am_today_in_utc - 1.day
+  describe "recent_activity_for(user:)" do
+    let(:current_time) { DateTime.now.utc }
+    let(:user) { create(:user) }
+    # notification_frequency set to 'hourly' by default
+    let(:subscription_tracker) { user.subscription_tracker }
 
-        Timecop.freeze(oakland_6am_yesterday_in_utc - 1.hours) do
-          create(:bucket, group: group, status: "draft")
-          create(:bucket, group: group, status: "live")
-          create(:bucket, group: group, status: "funded")
-        end
+    after { Timecop.return }
+    before do
+      subscription_tracker.update(recent_activity_last_fetched_at: current_time - 1.hour)
 
-        Timecop.freeze(oakland_6am_yesterday_in_utc + 2.hours) do
-          @d1 = create(:bucket, group: group, status: "draft")
-          @l1 = create(:bucket, group: group, status: "live")
-          @f1 = create(:bucket, group: group, status: "funded")
-        end
+      # # make some recent activity
+      # Timecop.freeze(current_time - 30.minutes) do
+      # end
 
-        Timecop.freeze(oakland_6am_yesterday_in_utc + 3.hours) do
-          @d2 = create(:bucket, group: group, status: "draft")
-          @l2 = create(:bucket, group: group, status: "live")
-          @f2 = create(:bucket, group: group, status: "funded")
-        end
+      # # make some old activity
+      # Timecop.freeze(current_time - 70.minutes) do
+      # end
+    end
+  end
 
-        Timecop.freeze(oakland_6am_today_in_utc + 1.hours) do
-          create(:bucket, group: group, status: "draft")
-          create(:bucket, group: group, status: "live")
-          create(:bucket, group: group, status: "funded")
-        end
-
-        @recent_activity = UserService.fetch_recent_activity_for(user: user)
-      end
-
-      after do
-        Timecop.return
-      end
-
-      it "returns users group" do
-        expect(@recent_activity[0][:group]).to eq(group)
-      end
-
-      it "returns users membership" do
-        expect(@recent_activity[0][:membership]).to eq(@membership)
-      end
-
-      it "returns all draft buckets created between 6am yesterday and 6am today (user's local time)" do
-        expect(@recent_activity[0][:draft_buckets].length).to eq(2)
-        expect(@recent_activity[0][:draft_buckets]).to include(@d1, @d2)
-      end
-
-      it "returns all buckets that became live between 6am yesterday and 6am today (user's local time)" do
-        expect(@recent_activity[0][:live_buckets].length).to eq(2)
-        expect(@recent_activity[0][:live_buckets]).to include(@l1, @l2)
-      end
-
-      it "returns all buckets that became funded between 6am yesterday and 6am today (user's local time)" do
-        expect(@recent_activity[0][:funded_buckets].length).to eq(2)
-        expect(@recent_activity[0][:funded_buckets]).to include(@f1, @f2)
+  describe "#comments_on_buckets_user_participated_in" do
+    context "user subscribed" do
+      it "returns all recent comments on buckets that user has participated in" do
       end
     end
 
-    context "user has multiple memberships in groups with recent activity" do
-      before do
-        make_user_group_member
-        user.update(utc_offset: -480)
-        utc_offset_in_hours = user.utc_offset / 60
-        oakland_6am_today_in_utc = (DateTime.now.in_time_zone(utc_offset_in_hours).beginning_of_day + 6.hours).utc
-        oakland_6am_yesterday_in_utc = oakland_6am_today_in_utc - 1.day
-
-        membership1 = create(:membership, member: user)
-        membership2 = create(:membership, member: user)
-        group1 = membership1.group
-        group2 = membership2.group
-
-        Timecop.freeze(oakland_6am_yesterday_in_utc + 2.hours) do
-          create(:bucket, group: group1, status: "draft")
-          create(:bucket, group: group1, status: "live")
-          create(:bucket, group: group1, status: "funded")
-        end
-
-        Timecop.freeze(oakland_6am_yesterday_in_utc + 3.hours) do
-          create(:bucket, group: group2, status: "draft")
-          create(:bucket, group: group2, status: "live")
-          create(:bucket, group: group2, status: "funded")
-        end
-
-        @recent_activity = UserService.fetch_recent_activity_for(user: user)
-      end
-
-      it "returns recent activity for every group user is a member of" do
-        expect(@recent_activity.length).to eq(2)
-      end
-    end
-
-    context "user has some archived memberships" do
-      before do
-        make_user_group_member
-        user.update(utc_offset: -480)
-        utc_offset_in_hours = user.utc_offset / 60
-        oakland_6am_today_in_utc = (DateTime.now.in_time_zone(utc_offset_in_hours).beginning_of_day + 6.hours).utc
-        oakland_6am_yesterday_in_utc = oakland_6am_today_in_utc - 1.day
-
-        membership1 = create(:membership, member: user)
-        membership2 = create(:membership, member: user, archived_at: DateTime.now.utc - 5.days)
-        group1 = membership1.group
-        group2 = membership2.group
-
-        Timecop.freeze(oakland_6am_yesterday_in_utc + 2.hours) do
-          create(:bucket, group: group1, status: "draft")
-          create(:bucket, group: group1, status: "live")
-          create(:bucket, group: group1, status: "funded")
-        end
-
-        Timecop.freeze(oakland_6am_yesterday_in_utc + 3.hours) do
-          create(:bucket, group: group2, status: "draft")
-          create(:bucket, group: group2, status: "live")
-          create(:bucket, group: group2, status: "funded")
-        end
-
-        @recent_activity = UserService.fetch_recent_activity_for(user: user)
-      end
-
-      it "returns recent activity for every group user is a member of" do
-        expect(@recent_activity.length).to eq(1)
-      end
-    end
-
-    context "user does not have a utc_offset specified yet" do
+    context "user not subscribed" do
       it "returns nil" do
-        make_user_group_member
-        user.update(utc_offset: nil)
-        expect(UserService.fetch_recent_activity_for(user: user)).to be_nil
       end
     end
 
-    context "there is no recent activity" do
+    context "no recent activity" do
       it "returns nil" do
-        make_user_group_member
-        user.update(utc_offset: -480)
-        expect(UserService.fetch_recent_activity_for(user: user)).to be_nil
+      end
+    end
+  end
+
+  describe "#comments_on_user_buckets" do
+    context "user subscribed" do
+      it "returns all recent comments on buckets that user has authored" do
+      end
+    end
+
+    context "user not subscribed" do
+      it "returns nil" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
+      end
+    end
+  end
+
+  describe "#contributions_to_users_buckets" do
+    context "user subscribed" do
+      it "returns all recent contributions to buckets that user has authored" do
+      end
+    end
+
+    context "user not subscribed" do
+      it "returns nil" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
+      end
+    end
+  end
+
+  describe "#contributions_to_buckets_user_participated_in" do
+    context "user subscribed" do
+      it "returns all recent contributions to buckets that user has participated in" do
+      end
+    end
+
+    context "user not subscribed" do
+      it "returns nil" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
+      end
+    end
+  end
+
+  describe "#users_buckets_fully_funded" do
+    context "user subscribed" do
+      it "returns all buckets the user has authored that have been recently funded" do
+      end
+    end
+
+    context "user not subscribed" do
+      it "returns nil" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
+      end
+    end
+  end
+
+  describe "#new_draft_buckets" do
+    context "user subscribed" do
+      it "returns all recently created bucket ideas in user's groups" do
+      end
+    end
+
+    context "user not subscribed" do
+      it "returns nil" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
+      end
+    end
+  end
+
+  describe "#new_live_buckets" do
+    context "user subscribed" do
+      it "returns all buckets in user's groups that have recently gone live" do
+      end
+    end
+
+    context "user not subscribed" do
+      it "returns nil" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
+      end
+    end
+  end
+
+  describe "#new_funded_buckets" do
+    context "user subscribed" do
+      it "returns all buckets in user's groups that have recently been fully funded" do
+      end
+    end
+
+    context "user not subscribed" do
+      it "returns nil" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
+      end
+    end
+  end
+
+  describe "#collated_activity_for_group(group)" do
+    context "recent activity exists" do
+      it "returns array containing group and activity hash" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
+      end
+    end
+  end
+
+  describe "#fetch_recent_activity_for(user:)" do
+    context "recent_activity exists" do
+      it "returns a hash where keys are the user's active groups, and values are activity hashes" do
+      end
+    end
+
+    context "no recent activity" do
+      it "returns nil" do
       end
     end
   end
