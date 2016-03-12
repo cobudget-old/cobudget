72e7a04cf716b753f0ee7f95f82c66a40633c0c2
euglazer <eugenestapler@gmail.com>
2016-03-11 20:06:43 -0800

    fix failing specs

--- app/models/subscription_tracker.rb
+++ app/models/subscription_tracker.rb
@@ -20,11 +20,11 @@ class SubscriptionTracker < ActiveRecord::Base
       if self.notification_frequency_changed?
         time_now = DateTime.now.utc
         datetime = case self.notification_frequency
-          when "hourly" then time_now.beginning_of_hour
-          when "daily" then (time_now.in_time_zone(self.user.utc_offset / 60).beginning_of_day + 6.hours)
-          when "weekly" then (time_now.in_time_zone(self.user.utc_offset / 60).beginning_of_week + 6.hours)
+          when "hourly" then time_now.beginning_of_hour.utc
+          when "daily" then (time_now.in_time_zone(self.user.utc_offset / 60).beginning_of_day + 6.hours).utc
+          when "weekly" then (time_now.in_time_zone(self.user.utc_offset / 60).beginning_of_week + 6.hours).utc
         end
-        self.update_columns(recent_activity_last_fetched_at: datetime.utc)
+        self.update_columns(recent_activity_last_fetched_at: datetime)
       end
     end
 end
