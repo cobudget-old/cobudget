517c7e1d0dfacbe37c9bcafd80f62f9b4e8c7800
euglazer <eugenestapler@gmail.com>
2016-04-10 03:08:08 -0700

    add contribution_count_data and unconfirmed_user_count to AnalyticsService

--- app/services/analytics_service.rb
+++ app/services/analytics_service.rb
@@ -5,20 +5,31 @@ class AnalyticsService
 
   def report
     {
-      user_count: user_count,
-      group_count: group_count
+      cumulative_user_count_data: cumulative_user_count_data,
+      cumulative_group_count_data: cumulative_group_count_data,
+      unconfirmed_user_count: unconfirmed_user_count,
+      contribution_count_data: contribution_count_data
     }
   end
 
-  def user_count
+  def cumulative_user_count_data
     data = User.group_by_day(:created_at).count
     total_users = 0
     data.map { |day, user_count| [day.strftime('%Y-%m-%d'), total_users += user_count]}.to_h
   end
 
-  def group_count
+  def cumulative_group_count_data
     data = Group.group_by_day(:created_at).count
     total_groups = 0
     data.map { |day, group_count| [day.strftime('%Y-%m-%d'), total_groups += group_count]}.to_h
+  end
+
+  def unconfirmed_user_count
+    User.where.not(confirmed_at: nil).count
+  end
+
+  def contribution_count_data
+    data = Contribution.group_by_day(:created_at).count
+    data.map { |day, contribution_count| [day.strftime('%Y-%m-%d'), contribution_count] }.to_h
   end
 end
--- spec/services/analytics_service_spec.rb
+++ spec/services/analytics_service_spec.rb
@@ -2,25 +2,17 @@ require 'rails_helper'
 
 describe "AnalyticsService" do
   describe "#report" do
-    describe "#user_count" do
+    describe "#cumulative_user_count_data" do
       it "works" do
-        Timecop.freeze(3.days.ago) do
-          create_list(:user, 2)
-        end
-
-        Timecop.freeze(2.days.ago) do
-          create_list(:user, 3)
-        end
-
-        Timecop.freeze(1.days.ago) do
-          create_list(:user, 5)
-        end
-
+        Timecop.freeze(3.days.ago) { create_list(:user, 2) }
+        Timecop.freeze(2.days.ago) { create_list(:user, 3) }
+        Timecop.freeze(1.days.ago) { create_list(:user, 5) }
         Timecop.return
+
         create_list(:user, 1)
 
-        user_count = AnalyticsService.report[:user_count]
-        expect(user_count).to eq({
+        cumulative_user_count_data = AnalyticsService.report[:cumulative_user_count_data]
+        expect(cumulative_user_count_data).to eq({
           3.days.ago.strftime('%Y-%m-%d') => 2,
           2.days.ago.strftime('%Y-%m-%d') => 5,
           1.days.ago.strftime('%Y-%m-%d') => 10,
@@ -29,29 +21,40 @@ describe "AnalyticsService" do
       end
     end
 
-    describe "#group_count" do
+    describe "#cumulative_group_count_data" do
       it "works" do
-        Timecop.freeze(3.days.ago) do
-          create_list(:group, 4)
-        end
-
-        Timecop.freeze(2.days.ago) do
-          create_list(:group, 2)
-        end
-
-        Timecop.freeze(1.days.ago) do
-          create_list(:group, 1)
-        end
-
+        Timecop.freeze(3.days.ago) { create_list(:group, 4) }
+        Timecop.freeze(2.days.ago) { create_list(:group, 2) }
+        Timecop.freeze(1.days.ago) { create_list(:group, 1) }
         Timecop.return
+
         create_list(:group, 5)
 
-        group_count = AnalyticsService.report[:group_count]
-        expect(group_count).to eq({
+        cumulative_group_count_data = AnalyticsService.report[:cumulative_group_count_data]
+        expect(cumulative_group_count_data).to eq({
           3.days.ago.strftime('%Y-%m-%d') => 4,
           2.days.ago.strftime('%Y-%m-%d') => 6,
           1.days.ago.strftime('%Y-%m-%d') => 7,
           DateTime.now.utc.strftime('%Y-%m-%d') => 12
+        })
+      end
+    end
+
+    describe "#contribution_count_data" do
+      it "works" do
+        Timecop.freeze(3.days.ago) { create_list(:contribution, 2) }
+        Timecop.freeze(2.days.ago) { create_list(:contribution, 5) }
+        Timecop.freeze(1.days.ago) { create_list(:contribution, 4) }
+        Timecop.return
+
+        create_list(:contribution, 1)
+
+        contribution_count_data = AnalyticsService.report[:contribution_count_data]
+        expect(contribution_count_data).to eq({
+          3.days.ago.strftime('%Y-%m-%d') => 2,
+          2.days.ago.strftime('%Y-%m-%d') => 5,
+          1.days.ago.strftime('%Y-%m-%d') => 4,
+          DateTime.now.utc.strftime('%Y-%m-%d') => 1
         })
       end
     end
