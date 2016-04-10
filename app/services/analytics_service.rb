557f05fa1da846f4c8abf7c361e59ed33799e356
euglazer <eugenestapler@gmail.com>
2016-04-09 19:25:40 -0700

    refactor analytics service

--- app/services/analytics_service.rb
+++ app/services/analytics_service.rb
@@ -1,7 +1,24 @@
 class AnalyticsService
-  def self.user_count_report
+  def self.report
+    self.new.report
+  end
+
+  def report
+    {
+      user_count: user_count,
+      group_count: group_count
+    }
+  end
+
+  def user_count
     data = User.group_by_day(:created_at).count
-    sum = 0
-    data.map { |date, count| [date.strftime('%Y-%m-%d'), sum += count]}.to_h
+    total_users = 0
+    data.map { |day, user_count| [day.strftime('%Y-%m-%d'), total_users += user_count]}.to_h
+  end
+
+  def group_count
+    data = Group.group_by_day(:created_at).count
+    total_groups = 0
+    data.map { |day, group_count| [day.strftime('%Y-%m-%d'), total_groups += group_count]}.to_h
   end
 end
--- spec/services/analytics_service_spec.rb
+++ spec/services/analytics_service_spec.rb
@@ -1,29 +1,59 @@
 require 'rails_helper'
 
 describe "AnalyticsService" do
-  describe "#user_count_report" do
-    it "works" do
-      Timecop.freeze(3.days.ago) do
-        create_list(:user, 2)
+  describe "#report" do
+    describe "#user_count" do
+      it "works" do
+        Timecop.freeze(3.days.ago) do
+          create_list(:user, 2)
+        end
+
+        Timecop.freeze(2.days.ago) do
+          create_list(:user, 3)
+        end
+
+        Timecop.freeze(1.days.ago) do
+          create_list(:user, 5)
+        end
+
+        Timecop.return
+        create_list(:user, 1)
+
+        user_count = AnalyticsService.report[:user_count]
+        expect(user_count).to eq({
+          3.days.ago.strftime('%Y-%m-%d') => 2,
+          2.days.ago.strftime('%Y-%m-%d') => 5,
+          1.days.ago.strftime('%Y-%m-%d') => 10,
+          DateTime.now.utc.strftime('%Y-%m-%d') => 11
+        })
       end
+    end
 
-      Timecop.freeze(2.days.ago) do
-        create_list(:user, 3)
+    describe "#group_count" do
+      it "works" do
+        Timecop.freeze(3.days.ago) do
+          create_list(:group, 4)
+        end
+
+        Timecop.freeze(2.days.ago) do
+          create_list(:group, 2)
+        end
+
+        Timecop.freeze(1.days.ago) do
+          create_list(:group, 1)
+        end
+
+        Timecop.return
+        create_list(:group, 5)
+
+        group_count = AnalyticsService.report[:group_count]
+        expect(group_count).to eq({
+          3.days.ago.strftime('%Y-%m-%d') => 4,
+          2.days.ago.strftime('%Y-%m-%d') => 6,
+          1.days.ago.strftime('%Y-%m-%d') => 7,
+          DateTime.now.utc.strftime('%Y-%m-%d') => 12
+        })
       end
-
-      Timecop.freeze(1.days.ago) do
-        create_list(:user, 5)
-      end
-
-      Timecop.return
-      create_list(:user, 1)
-
-      expect(AnalyticsService.user_count_report).to eq({
-        3.days.ago.strftime('%Y-%m-%d') => 2,
-        2.days.ago.strftime('%Y-%m-%d') => 5,
-        1.days.ago.strftime('%Y-%m-%d') => 10,
-        DateTime.now.utc.strftime('%Y-%m-%d') => 11
-      })
     end
   end
 end
