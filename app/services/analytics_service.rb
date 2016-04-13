fca83d0caed6e7cdb8262b2cd75d55ae639ea168
euglazer <eugenestapler@gmail.com>
2016-04-12 17:11:39 -0700

    tailor analytics report format for chartjs

--- app/services/analytics_service.rb
+++ app/services/analytics_service.rb
@@ -17,18 +17,19 @@ class AnalyticsService
     def cumulative_user_count_data
       data = User.group_by_day(:created_at).count
       total_users = 0
-      x, y = [], []
-      data.each do |day, user_count|
-        x << day.strftime('%Y-%m-%d')
-        y << total_users += user_count
-      end
-      {x: x, y: y}
+      {
+        x: data.keys.map { |day| day.strftime('%Y-%m-%d') },
+        y: data.values.map { |user_count| total_users += user_count }
+      }
     end
 
     def cumulative_group_count_data
       data = Group.group_by_day(:created_at).count
       total_groups = 0
-      data.map { |day, group_count| [day.strftime('%Y-%m-%d'), total_groups += group_count]}.to_h
+      {
+        x: data.keys.map { |day| day.strftime('%Y-%m-%d') },
+        y: data.values.map { |group_count| total_groups += group_count }
+      }
     end
 
     def unconfirmed_user_count
@@ -37,14 +38,17 @@ class AnalyticsService
 
     def contribution_count_data
       data = Contribution.group_by_day(:created_at).count
-      data.map { |day, contribution_count| [day.strftime('%Y-%m-%d'), contribution_count] }.to_h
+      {
+        x: data.keys.map { |day| day.strftime('%Y-%m-%d') },
+        y: data.values
+      }
     end
 
     def group_data
       sorted_groups = Group.find_each.sort_by { |group| group.last_activity_at }.reverse
       sorted_groups.map do |group|
         {
-          admins: group.members.joins(:memberships).where(memberships: {is_admin: true}).order(:created_at).as_json(only: [:name, :email]).map { |h| h.symbolize_keys },
+          admins: group.members.joins(:memberships).where(memberships: {is_admin: true}).distinct.order(:created_at).as_json(only: [:name, :email]).map { |h| h.symbolize_keys },
           id: group.id,
           created_at: group.created_at,
           last_activity_at: group.last_activity_at,
