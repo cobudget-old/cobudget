ebab1a60f417267b40e206588d0bd56b5a0c12b5
euglazer <eugenestapler@gmail.com>
2016-04-13 18:15:41 -0700

    clean up migration history

--- db/migrate/20160311100322_create_subscription_trackers.rb
+++ db/migrate/20160311100322_create_subscription_trackers.rb
@@ -2,26 +2,9 @@ class CreateSubscriptionTrackers < ActiveRecord::Migration
   def up
     create_table :subscription_trackers do |t|
       t.belongs_to :user, index: true, null: false
-
-      t.boolean :comment_on_your_bucket, default: true, null: false
-      t.boolean :comment_on_bucket_you_participated_in, default: true, null: false
-
-      t.boolean :bucket_idea_created, default: true, null: false
-      t.boolean :bucket_started_funding, default: true, null: false
-      t.boolean :bucket_fully_funded, default: true, null: false
-
-      t.boolean :funding_for_your_bucket, default: true, null: false
-      t.boolean :funding_for_a_bucket_you_participated_in, default: true, null: false
-      t.boolean :your_bucket_fully_funded, default: true, null: false
-
-      t.datetime :recent_activity_last_fetched_at
-      t.string :notification_frequency, default: "hourly", null: false
-
+      t.boolean :subscribed_to_email_notifications, default: false
+      t.string :email_digest_delivery_frequency, default: "never"
       t.timestamps null: false
-    end
-
-    User.all.each do |user|
-      SubscriptionTracker.create(user: user, recent_activity_last_fetched_at: DateTime.now.utc.beginning_of_hour) unless user.subscription_tracker
     end
   end
 
