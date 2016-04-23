53cd2771d88530798b59eb3583fbbfdc02ad0a0b
euglazer <eugenestapler@gmail.com>
2016-04-22 18:37:12 -0700

    omit cobudget alpha ghost from analytics

--- app/services/group_analytics_service.rb
+++ app/services/group_analytics_service.rb
@@ -1,6 +1,6 @@
 class GroupAnalyticsService
   attr_reader :group
-  attr_accessor :group_bucket_ids, :confirmed_group_member_ids
+  attr_accessor :group_bucket_ids, :confirmed_group_member_ids, :cobudget_alpha_lol
 
   def self.report(group)
     self.new(group).report
@@ -30,7 +30,7 @@ class GroupAnalyticsService
     end
 
     def contribution_amounts_per_day
-      data = Contribution.where(bucket_id: group_bucket_ids).group_by_day(:created_at).sum(:amount)
+      data = Contribution.where(bucket_id: group_bucket_ids).where.not(user: cobudget_alpha_lol).group_by_day(:created_at).sum(:amount)
       {
         x: data.keys.map { |day| day.strftime('%Y-%m-%d') },
         y: data.values
@@ -38,7 +38,7 @@ class GroupAnalyticsService
     end
 
     def allocation_amounts_per_day
-      data = group.allocations.group_by_day(:created_at).sum(:amount)
+      data = group.allocations.where.not(user: cobudget_alpha_lol).group_by_day(:created_at).sum(:amount)
       {
         x: data.keys.map { |day| day.strftime('%Y-%m-%d') },
         y: data.values
@@ -46,7 +46,7 @@ class GroupAnalyticsService
     end
 
     def comment_counts_per_day
-      data = Comment.where(bucket_id: group_bucket_ids).group_by_day(:created_at).count
+      data = Comment.where(bucket_id: group_bucket_ids).where.not(user: cobudget_alpha_lol).group_by_day(:created_at).count
       {
         x: data.keys.map { |day| day.strftime('%Y-%m-%d') },
         y: data.values
@@ -59,5 +59,9 @@ class GroupAnalyticsService
 
     def confirmed_group_member_ids
       confirmed_group_member_ids ||= group.members.where.not(confirmed_at: nil).pluck(:id)
+    end
+
+    def cobudget_alpha_lol
+      cobudget_alpha_lol ||= User.find_by_email("alphaghost@cobudget.co")
     end
 end
