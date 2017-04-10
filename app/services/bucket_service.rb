25684eaf4d6a73c48dda54c43f0cadea8cf2edd4
jlopker <jlopker@gmail.com>
2017-04-10 15:15:07 -0700

    new tests for paid route and refunded status

--- app/services/bucket_service.rb
+++ app/services/bucket_service.rb
@@ -1,7 +1,7 @@
 class BucketService
   def self.archive(bucket:, exclude_author_from_email_notifications: false)
     bucket.update(archived_at: DateTime.now.utc)
-    if bucket.status == 'live' or 'funded'
+    if bucket.status == 'live' || bucket.status == 'funded'
       bucket.contributors(exclude_author: exclude_author_from_email_notifications).each do |funder|
         UserMailer.notify_funder_that_bucket_was_archived(funder: funder, bucket: bucket).deliver_now
       end
