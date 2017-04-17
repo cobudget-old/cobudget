04468f68ec56957ff3d9194c1eec225c44a5bf48
jlopker <jlopker@gmail.com>
2017-04-17 14:53:13 -0700

    after logging in, show the group that had the most recent activity

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -7,10 +7,11 @@ class GroupSerializer < ActiveModel::Serializer
              :currency_code,
              :is_launched,
              :plan,
-             :trial_end, 
+             :trial_end,
              :total_in_circulation,
              :ready_to_pay_total,
-             :total_in_funded, 
+             :total_in_funded,
              :total_allocations,
-             :total_contributions
+             :total_contributions,
+             :last_activity_at
 end
