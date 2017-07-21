0a960091286f2648357637cfa98602f8a859618e
jlopker <jlopker@gmail.com>
2017-07-21 12:47:54 +0200

    controller file typo

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,8 +1,8 @@
-class AnnouncementController < AuthenticatedController
+class AnnouncementsController < AuthenticatedController
   api :GET, '/announcements', 'Get a list of Announcements for this user'
   def index
-    announcements = Announcements.all
+    announcements = Announcement.all
     render json: announcements
   end
- 
+
 end
