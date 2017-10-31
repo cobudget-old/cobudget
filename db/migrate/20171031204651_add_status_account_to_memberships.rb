b1e8650477b687e437d81199b78a383ebe3e776e
Michael Arnoldus <chime@mu.dk>
2017-11-01 00:29:39 +0100

    Added code to bucket and membership migrations so the new account field gets populated with a new account

--- db/migrate/20171031204651_add_status_account_to_memberships.rb
+++ db/migrate/20171031204651_add_status_account_to_memberships.rb
@@ -2,5 +2,14 @@ class AddStatusAccountToMemberships < ActiveRecord::Migration
   def change
     add_column :memberships, :status_account_id, :integer
     add_foreign_key :memberships, :accounts, column: :status_account_id
+
+    # Create accounts for the new field
+    Membership.find_each do |membership|
+      account = Account.new({group_id: membership.group_id})
+      if account.save
+        membership.status_account_id = account.id
+        membership.save!
+      end
+    end
   end
 end
--- db/migrate/20171031204735_add_account_ref_to_buckets.rb
+++ db/migrate/20171031204735_add_account_ref_to_buckets.rb
@@ -2,5 +2,15 @@ class AddAccountRefToBuckets < ActiveRecord::Migration
   def change
     add_column :buckets, :account_id, :integer
     add_foreign_key :buckets, :accounts
+
+    # Create accounts for the new field
+    Bucket.find_each do |bucket|
+      account = Account.new({group_id: bucket.group_id})
+      if account.save
+        bucket.account_id = account.id
+        bucket.save!
+      end
+    end
+
   end
 end
