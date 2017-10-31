71530eb06b37520ad027856e19542cd7c4d30063
Michael Arnoldus <chime@mu.dk>
2017-10-31 23:00:05 +0100

    Reverting changes added to the wrong branch

--- db/migrate/20171031204735_add_account_ref_to_buckets.rb
+++
@@ -1,6 +0,0 @@
-class AddAccountRefToBuckets < ActiveRecord::Migration
-  def change
-    add_column :buckets, :account_id, :integer
-    add_foreign_key :buckets, :accounts
-  end
-end
--- db/migrate/20171031213553_add_foreign_keys_to_transactions.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeysToTransactions < ActiveRecord::Migration
-  def change
-    add_foreign_key :transactions, :accounts, column: :from_account_id
-    add_foreign_key :transactions, :accounts, column: :to_account_id
-  end
-end
