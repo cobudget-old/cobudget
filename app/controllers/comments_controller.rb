a87bd8bef8ff0bd292cb7baee129d0e24c389947
jlopker <jlopker@gmail.com>
2018-01-19 16:57:32 -0800

    make email body text html_safe

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < AuthenticatedController
     new_body = ReverseMarkdown.convert body
 
     comment = Comment.create(body: new_body, user_id: comment_params[:user_id], bucket_id: comment_params[:bucket_id])
-    noko_body = Nokogiri::HTML(body)
+    noko_body = Nokogiri::HTML.fragment(body)
     user_links = noko_body.css("a").map {|a|
       a.attributes.values[1].value
     }
@@ -22,15 +22,12 @@ class CommentsController < AuthenticatedController
       a.attributes["href"].value = "#{Rails.application.config.action_mailer.default_url_options[:host]}/##{a['href']}"
     end
 
-    email_body = noko_body.children.to_html
-
     if user_links
-      # turn the body markdown text into html for sending in an email
-      body_html = markdown_as_html(email_body)
+      email_body = noko_body.to_html.html_safe
       bucket = Bucket.find(comment_params[:bucket_id])
       user_links.each do |userId|
         user = User.find(userId)
-        UserMailer.notify_member_that_they_were_mentioned(author: current_user, member: user, bucket: bucket, body: body_html).deliver_later
+        UserMailer.notify_member_that_they_were_mentioned(author: current_user, member: user, bucket: bucket, body: email_body).deliver_now
       end
     end
     if comment.valid?
@@ -53,12 +50,5 @@ class CommentsController < AuthenticatedController
       return @group if @group
       bucket_id = params[:bucket_id] || comment_params[:bucket_id]
       @group = Bucket.find_by_id(bucket_id).group if bucket_id
-    end
-
-    # takes markdown and renders as html
-    def markdown_as_html(body)
-      renderer = Redcarpet::Render::HTML.new
-      markdown = Redcarpet::Markdown.new(renderer, extensions = {})
-      markdown.render(body).html_safe
     end
 end
