a255dec6bb683b2c5e65d8ecf0030cf9314cb081
Gardner Bickford <gardner@bickford.nz>
2020-05-31 04:54:12 +1200

    Adding Sentry

--- api/config/initializers/filter_parameter_logging.rb
+++ api/config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,8 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+
+# Sentry ignore fields
+Raven.configure do |config|
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+end
